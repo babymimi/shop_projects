739415310bc3621498588356a3292cbe149d3824
babymimi <593078940>
2018-09-18 00:54:02 +0800

    代码改进

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import *
+from apps.home.models import *
 
 
 def index(request):
